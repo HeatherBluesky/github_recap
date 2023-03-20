98caf54e036bdc3c02fdf627134292229008d606
Heather <heather@Kierans-MacBook.local>
2023-03-20 15:28:26 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,13 @@
+#variables 
+number_of_students = 16 
+number_of_instructors = 3
+weeks_of_course = 16
+
+current_week = week_one
+day_of_current_week = one 
+subject_of_the_day = PythonBasics
+
+print(current_week + day_of_current_week +subject_of_the_day)
+people_in_classroom = number_of_instructors + number_of_students
+
+
